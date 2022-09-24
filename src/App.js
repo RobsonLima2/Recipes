b7b6edccb35fd064c3933a25124fb1998fb6ccea
Luizgsmkw <luizgustavokw@gmail.com>
2022-09-24 11:10:58 -0300

    finalização da arquitetura

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 
+import { GlobalStyle } from "./styles/styledGlobal";
 function App() {
   return (
     <div>
-      <h1>Texto</h1>
+      <GlobalStyle/>
+      <h1>RC</h1>
     </div>
   );
 }
