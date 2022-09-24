4d5e85aa066435d3fb24fb0b77ec83b9ce8bf1d7
RobsonLima2 <robson.ramos.lima@hotmail.com>
2022-09-24 10:23:43 -0300

    add estrutura header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import foto from "../assets/"
+
+function Header() {
+  return (
+    <div>
+      <img src={foto} alt=""/>
+    </div>
+  );
+}
+
+export default Header;
