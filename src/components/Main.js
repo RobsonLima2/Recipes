858b1beb0a9717c23ce9285fefbea3f1a67f8ce2
RobsonLima2 <robson.ramos.lima@hotmail.com>
2022-09-25 12:29:23 -0300

    finalização main

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,8 +3,9 @@ import * as S from "../styles/styledMain";
 import cake from '../assets/foodiesfeed.com_detail-of-pavlova-strawberry-piece-of-cake.png'
 import pizza from '../assets/foodiesfeed.com_neapolitan-pizza-margherita.png'
 import smoothie from '../assets/foodiesfeed.com_peanut-butter-and-banana-smoothie.png'
+
 const Main = () => {
-  
+
   return (
     <main>
       <S.SectionRecipes>
@@ -38,6 +39,23 @@ const Main = () => {
           </S.ContainerCards>
         </S.Container>
       </S.SectionRecipes>
+      <S.SectionAbout>
+        <S.DivImgAbout></S.DivImgAbout>
+        <S.DivTextAbout>
+          <S.DivAbout>
+          <S.H2About>ABOUT</S.H2About>
+          <S.LinhaAbout></S.LinhaAbout>
+          </S.DivAbout>
+          <S.PTextAbout>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tincidunt, tortor nec rhoncus dictum, lorem massa tempus sem, eu tincidunt libero velit sit amet velit. Nunc in euismod urna. Duis dapibus, elit eu eleifend tincidunt, nulla ipsum consectetur lorem, quis tempor lorem justo quis nisi. Nam interdum, nisi nec mollis sagittis, enim risus euismod nisi, quis rutrum quam augue id mauris. Pellentesque mattis hendrerit semper. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut vestibulum nisl ante, et ultricies sapien facilisis aliquam.</S.PTextAbout>
+        </S.DivTextAbout>
+
+      </S.SectionAbout>
+      <S.SectionSubs>
+        <S.H2Subs>SUBSCRIBE</S.H2Subs>
+        <S.PSubs>Sign up for newsletter</S.PSubs>
+        <S.InputSubs type='email' placeholder="Your Email"/>
+        <S.BtnSubs>SUBMIT</S.BtnSubs>
+      </S.SectionSubs>
     </main>
   );
 };
--- src/styles/styledMain.js
+++ src/styles/styledMain.js
@@ -104,3 +104,173 @@ export const Linha = styled.div `
         border-radius: 10px;
         margin-bottom: 34.5px;
 `
+
+export const SectionAbout = styled.section `
+display: flex;
+
+@media(max-width:1160px ){
+    flex-direction: column;
+    
+}
+
+`
+export const DivImgAbout = styled.div`
+width: 50%;
+height: 957px;
+background-image: url('https://raw.githubusercontent.com/RobsonLima2/Recipes/main/src/assets/pexels-karolina-grabowska-4199094.png');
+background-position: center;
+
+@media(max-width:1160px ){
+    width: 100%;
+    
+}
+
+@media(max-width:515px ){
+    height: 413px;
+    background-size: cover;
+    background-repeat: no-repeat;
+    
+}
+
+`
+
+export const DivTextAbout = styled.div `
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+width: 50%;
+height: 957px;
+
+@media(max-width:1160px ){
+    width: 100%;
+
+    
+}
+`
+export const PTextAbout = styled.p `
+width: 576px;
+text-align: justify;
+font-size: 20px;
+line-height: 35px;
+
+@media(max-width:1160px ){
+    width: 700px;
+    
+}
+
+@media(max-width:720px ){
+   width: 500px;
+    
+}
+
+@media(max-width:520px ){
+   width: 330px;
+   font-size:16px ;
+}
+
+
+`
+export const DivAbout = styled.div `
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+margin-bottom: 94.5px;
+`
+
+export const LinhaAbout = styled.div `
+
+        border-bottom: 5px solid black;       
+        width:80px;
+        height: 8px;
+        text-align: center;
+        margin-top: 15px;        
+
+`
+
+export const H2About = styled.h2 `
+font-size:40px;
+`
+
+export const SectionSubs = styled.section `
+background-color: #DFE4DE;
+height: 764px;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+`
+
+export const H2Subs = styled.h2 `
+font-size: 40px;
+letter-spacing: 4px;
+font-weight: 500;
+margin-bottom: 13px;
+
+@media(max-width:410px ){
+   
+   font-size: 30px;
+    
+}
+`
+
+export const PSubs = styled.p `
+font-size: 32px;
+letter-spacing: 4px;
+font-weight: 400;
+margin-bottom: 42px;
+
+@media(max-width:410px ){
+   
+   font-size: 20px;
+    
+}
+`
+
+export const InputSubs = styled.input `
+width: 792px;
+height: 120px;
+background-color: #EFF1EE;
+border: none;
+padding-left: 50px;
+font-size: 24px;
+margin-bottom: 40px;
+
+@media(max-width:800px ){
+   width: 382px;
+   height: 74px;
+   padding-left: 37px;
+   font-size: 20px;
+    
+}
+
+@media(max-width:410px ){
+   
+    width: 330px;
+    
+}
+
+
+`
+export const BtnSubs = styled.button`
+width: 240px;
+height: 81px;
+background-color: transparent;
+border: 5px solid #373737;
+font-size: 26px;
+letter-spacing: 6px;
+
+@media(max-width:800px ){
+   width: 382px;
+   height: 74px;
+   font-size: 24px;
+}
+
+@media(max-width:410px ){
+   
+   width: 330px;
+   
+}
+
+`
